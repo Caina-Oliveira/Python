efe18c2a55bf72451163bbc9319d0272922eea9b
Caina-Oliveira <caina32@gmail.com>
2023-10-24 16:56:57 -0300

    Limpeza

--- ecosystem.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  apps : [{
-    name   : "jolie",
-    script : "dist/index.js"
-  }]
-}
--- src/config/queue.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const queue = axios.create({
-  baseURL: 'https://prod.izicash.app/skynet',
-});
-
-export default queue;
